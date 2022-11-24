036dfb92efe09ee66510e79682c7dbfefecfaf99
vluke123 <vluke123@hotmail.co.uk>
2022-11-24 19:58:59 +0000

    back home

--- app.py
+++ app.py
@@ -1,29 +1,62 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, jsonify, abort
 from flask_sqlalchemy import SQLAlchemy
+import sys
 
 app = Flask(__name__) # __name__ just allows us to tell Flask name of file
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres@localhost:5432/todos'
-db = SQLAlchemy(app)
+db = SQLAlchemy(app, session_options={"expire_on_commit": False})
 
 class Todo(db.Model):
+    __tablename__ = 'todos'
     id = db.Column(db.Integer, primary_key = True)
     description = db.Column(db.String(), nullable = False)
+    completed = db.Column(db.Boolean(), nullable = False)
 
     def __repr__(self):
-        return f'<Todo {self.id} {self.description}'
+        return f'<Todo {self.id} {self.description}>'
 
 db.create_all()
 
 @app.route('/') 
 def index(): 
-    return render_template('index.html', data = Todo.query.all())
+    return render_template('index.html', data=Todo.query.order_by('id').all())
 
 @app.route('/create', methods=['POST'])
 def create_todo():
-    description = request.form.get('description', '')
-    create_row = Todo(description=description)
-    db.session.add(create_row)
-    db.session.commit()
-    return redirect(url_for('index'))
+    error = False
+    body = {}
+    try:
+        description = request.get_json()['description']
+        print(description)
+        create_row = Todo(description=description, completed=False)
+        db.session.add(create_row)
+        db.session.commit()
+        body['id'] = create_row.id
+        body['completed'] = create_row.completed
+        body['description'] = create_row.description
+    except:
+        error = True
+        db.session.rollback()
+        print(sys.exc_info())
+    finally:
+        db.session.close()
+    if error:
+        abort(400)
+    else:
+        return jsonify(body)
 
+
+@app.route('/todos/<todo_id>/set-completed', methods=['POST'])
+def set_completed_todos(todo_id):
+    try:
+        completed = request.get_json()['completed']
+        print('completed', completed)
+        todo = Todo.query.get(todo_id)
+        todo.completed = completed
+        db.session.commit()
+    except:
+        db.session.rollback()
+    finally:
+        db.session.close()
+    return redirect(url_for('index'))
 
